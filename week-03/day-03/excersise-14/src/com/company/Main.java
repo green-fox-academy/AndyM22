2003697fc201f28191df586da53adf96b5b8c56c
Andrea Malinová <an.malinova@seznam.cz>
2020-06-04 21:59:26 +0200

    Add excersise-14

---
+++ week-03/day-03/excersise-14/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int baseNum = 123;
+        int sum = doubling(baseNum);
+        System.out.println(sum);
+
+    }
+
+    public static int doubling(int a) {
+        return (a * a);
+
+    }
+}
