c8c22a42950e45cede9837e68bea3accd1aa380a
Andrea Malinová <an.malinova@seznam.cz>
2020-06-04 21:55:13 +0200

    Add excersise 04-06

---
+++ week-03/day-03/excersise-04/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int numbers [] = new int [] {1, 2, 3, 8, 5, 6};
+
+        numbers[4] = 8;
+
+        System.out.println(numbers[3]);
+
+
+    }
+}
---
+++ week-03/day-03/excersise-05/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int [] numbers = new int [] {1, 2, 3, 4, 5};
+
+        numbers[2] = numbers[2] +1;
+
+        System.out.println(numbers[2]);
+
+
+    }
+}
