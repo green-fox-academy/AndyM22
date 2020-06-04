9f24de70e371580467d1ef528e189671bc423915
Andrea Malinová <an.malinova@seznam.cz>
2020-06-05 00:27:10 +0200

    Add excersise-18

--- week-03/day-03/excersise-18/src/com/company/Main.java
+++ week-03/day-03/excersise-18/src/com/company/Main.java
@@ -5,12 +5,12 @@ public class Main {
     public static void main(String[] args) {
         int firstNumber = 1;
         int secondNumber = 5;
-        int result = sum(firstNumber, secondNumber);
+        int result = factorio(firstNumber, secondNumber);
         System.out.println(result);
 
     }
 
-    public static int sum(int a, int b) {
+    public static int factorio(int a, int b) {
         int sumNumber = 1;
         for (a = 1; a <= b; a++) {
             sumNumber = sumNumber * a;
