b3e8a5ca636726af63dc39a042b15439ec1695dc
Andrea Malinová <an.malinova@seznam.cz>
2020-06-06 12:10:43 +0200

    Add excersise-01

---
+++ week-03/day-05/excersise-01/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String example = "In a dishwasher far far away";
+
+        example = example.replace("dishwasher", "galay");
+
+        System.out.println(example);
+
+
+    }
+}
