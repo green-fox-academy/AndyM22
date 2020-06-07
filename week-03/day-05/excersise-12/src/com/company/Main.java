8c6ecc19b9dd7861fb8a1ca35632e4ca8486203f
Andrea Malinová <an.malinova@seznam.cz>
2020-06-08 00:16:11 +0200

    Add excersise-12

---
+++ week-03/day-05/excersise-12/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> shoppingList = new ArrayList<>();
+
+        shoppingList.add("eggs");
+        shoppingList.add("milk");
+        shoppingList.add("fish");
+        shoppingList.add("bread");
+        shoppingList.add("chicken");
+
+        if (shoppingList.contains("milk")) {
+            System.out.println("Yes, we have milk on the list");
+        } else {
+            System.out.println("No, we don't have milk on the list");
+        }
+
+        if (shoppingList.contains("bananas")) {
+            System.out.println("Yes, we have bananas on the list");
+        } else {
+            System.out.println("No, we don't have bananas on the list");
+        }
+
+    }
+}
