1613a86c8157268b157336f1df89f02c7a6f3973
Andrea Malinová <an.malinova@seznam.cz>
2020-06-08 00:07:41 +0200

    Add excersise-11

---
+++ week-03/day-05/excersise-11/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HashMap<String, String> telephoneBook = new HashMap<>();
+
+        telephoneBook.put("William A. Lathan", "405-709-1865");
+        telephoneBook.put("John K. Miller", "402-247-8568");
+        telephoneBook.put("Hortensia E. Foster", "606-481-6467");
+        telephoneBook.put("Amanda D. Newland", "319-243-5613");
+        telephoneBook.put("Brooke P. Askew", "307-687-2982");
+
+        System.out.println("John K. Miller's phone number is: " + telephoneBook.get("John K. Miller"));
+
+        System.out.println("The number 307-687-2982 belongs to Brooke P. Askew");
+
+        if (telephoneBook.containsKey("Chris E. Myer")) {
+            System.out.println("Telephone Book contains Chris E. Myers' phone number");
+        } else {
+            System.out.println("Telephone Book does not contain Chris E. Myers' phone number");
+        }
+
+    }
+
+}
