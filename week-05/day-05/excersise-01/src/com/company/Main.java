20c61161de3966cb8008d45f55d8dba781db04a2
Andrea Malinová <an.malinova@seznam.cz>
2020-06-20 13:39:43 +0200

    DoableHomework

---
+++ week-05/day-05/excersise-01/src/com/company/Main.java
@@ -0,0 +1,76 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //First tree
+
+        ArrayList<String> firstTree = new ArrayList<>();
+        String type1 = "";
+        String leafColor1 = "";
+        int number1 = 0;
+        String age1 = "" + number1;
+        String sex1 = "";
+        firstTree.add(type1);
+        firstTree.add(leafColor1);
+        firstTree.add(age1);
+        firstTree.add(sex1);
+
+        //Second Tree
+
+        ArrayList<String> secondTree = new ArrayList<>();
+        String type2 = "";
+        String leafColor2 = "";
+        int number2 = 0;
+        String age2 = "" + number2;
+        String sex2 = "";
+        secondTree.add(type2);
+        secondTree.add(leafColor2);
+        secondTree.add(age2);
+        secondTree.add(sex2);
+
+        //Third Tree
+
+        ArrayList<String> thirdTree = new ArrayList<>();
+        String type3 = "";
+        String leafColor3 = "";
+        int number3 = 0;
+        String age3 = "" + number3;
+        String sex3 = "";
+        thirdTree.add(type3);
+        thirdTree.add(leafColor3);
+        thirdTree.add(age3);
+        thirdTree.add(sex3);
+
+        //Fourth Tree
+
+        ArrayList<String> fourthTree = new ArrayList<>();
+        String type4 = "";
+        String leafColor4 = "";
+        int number4 = 0;
+        String age4 = "" + number4;
+        String sex4 = "";
+        fourthTree.add(type4);
+        fourthTree.add(leafColor4);
+        fourthTree.add(age4);
+        fourthTree.add(sex4);
+
+        //Fifth Tree
+
+        ArrayList<String> fifthTree = new ArrayList<>();
+        String type5 = "";
+        String leafColor5 = "";
+        int number5 = 0;
+        String age5 = "" + number5;
+        String sex5 = "";
+        fifthTree.add(type5);
+        fifthTree.add(leafColor5);
+        fifthTree.add(age5);
+        fifthTree.add(sex5);
+
+
+    }
+}
