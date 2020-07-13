120560d447d7f5e7dafd46f647e0256d21bc60c1
Andrea Malinová <an.malinova@seznam.cz>
2020-07-13 22:22:54 +0200

    CompareLength

--- week-03/day-03/excersise-02/src/com/company/Main.java
+++ week-03/day-03/excersise-02/src/com/company/Main.java
@@ -4,17 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        int [] firstArrayOfNumbers = new int [] {1, 2, 3};
-        int [] secondArrayOfNumbers = new int [] {4, 5};
+        int[] firstArrayOfNumbers = new int[]{1, 2, 3};
+        int[] secondArrayOfNumbers = new int[]{4, 5};
 
         if (firstArrayOfNumbers.length < secondArrayOfNumbers.length) {
             System.out.println("secondArrayOfNumbers is longer");
         } else {
             System.out.println();
-
         }
-
 
     }
 }
