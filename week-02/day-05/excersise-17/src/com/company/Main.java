938b8ff5bed525d4e15b25b2e51f88f13875e3c4
Andrea Malinová <an.malinova@seznam.cz>
2020-07-13 20:44:11 +0200

    IWontCheatOnTheExams

--- week-02/day-05/excersise-17/src/com/company/Main.java
+++ week-02/day-05/excersise-17/src/com/company/Main.java
@@ -3,13 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-
 
         for (int i = 1; i < 100; i++) {
             System.out.println("I won't cheat on the exam!");
         }
-
-
+        
     }
 }
