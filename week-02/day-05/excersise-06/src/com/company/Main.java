2151287896091c11f8c15992998f49812661d5d4
Andrea Malinová <an.malinova@seznam.cz>
2020-07-13 20:31:23 +0200

    CodingHours

--- week-02/day-05/excersise-06/src/com/company/Main.java
+++ week-02/day-05/excersise-06/src/com/company/Main.java
@@ -3,18 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
         int dailyHours = 6;
-        int semesterHour = (17*5*6);
+        int semesterHour = (17 * 5 * 6);
 
         System.out.println("Attendee working on workdays: " + semesterHour);
 
         float weeklyHours = 52f;
-        float allHours = (24*7*17f);
+        float allHours = (24 * 7 * 17f);
         System.out.println("Percentage of coding hours: " + ((weeklyHours * 100) / allHours) + "%");
-
-
 
     }
 }
