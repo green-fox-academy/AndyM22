564b2561a73b6894379ec73182182ec9c0295cec
Andrea Malinová <an.malinova@seznam.cz>
2020-07-13 22:30:54 +0200

    ChangeElement

--- week-03/day-03/excersise-04/src/com/company/Main.java
+++ week-03/day-03/excersise-04/src/com/company/Main.java
@@ -4,13 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int numbers[] = new int[]{1, 2, 3, 8, 5, 6};
 
-        int numbers [] = new int [] {1, 2, 3, 8, 5, 6};
-
-        numbers[4] = 8;
+        numbers[3] = 4;
 
         System.out.println(numbers[3]);
-
 
     }
 }
