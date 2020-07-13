40f94c57c7795a2533806f150267c9a9ecc578d9
Andrea Malinová <an.malinova@seznam.cz>
2020-07-13 20:33:20 +0200

    Swap

--- week-02/day-05/excersise-08/src/com/company/Main.java
+++ week-02/day-05/excersise-08/src/com/company/Main.java
@@ -3,19 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
         int a = 123;
         int b = 526;
-
         int c = a;
-
         a = b;
         b = c;
 
         System.out.println("a = " + a);
         System.out.println("b = " + b);
-
-
+        
     }
 }
