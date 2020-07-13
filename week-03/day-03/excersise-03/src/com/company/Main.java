9c96d4eb71a7ec4748053e4111d578efef814c58
Andrea Malinová <an.malinova@seznam.cz>
2020-07-13 22:28:11 +0200

    SumElements

--- week-03/day-03/excersise-03/src/com/company/Main.java
+++ week-03/day-03/excersise-03/src/com/company/Main.java
@@ -4,15 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int[] numbers = new int[]{54, 23, 66, 12};
 
-
-        int [] numbers = new int [] {54, 23, 66, 12};
-
-        int result = numbers[2] + numbers[3];
+        int result = numbers[1] + numbers[2];
 
         System.out.println(result);
-
-
 
     }
 }
