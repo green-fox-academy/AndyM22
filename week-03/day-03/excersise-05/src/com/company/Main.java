d8d1669cb85d0f1370aee9c204de336bdd183ba5
Andrea Malinová <an.malinova@seznam.cz>
2020-07-13 22:32:25 +0200

    IncrementElement

--- week-03/day-03/excersise-05/src/com/company/Main.java
+++ week-03/day-03/excersise-05/src/com/company/Main.java
@@ -4,13 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int[] numbers = new int[]{1, 2, 3, 4, 5};
 
-        int [] numbers = new int [] {1, 2, 3, 4, 5};
-
-        numbers[2] = numbers[2] +1;
+        numbers[2] = numbers[2] + 1;
 
         System.out.println(numbers[2]);
-
 
     }
 }
