d7990617b45da18cde704494d68d017100135f16
Andrea Malinová <an.malinova@seznam.cz>
2020-07-13 22:14:58 +0200

    Third

--- week-03/day-03/excersise-01/src/com/company/Main.java
+++ week-03/day-03/excersise-01/src/com/company/Main.java
@@ -3,13 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
+        int[] numbers = new int[]{4, 5, 6, 7};
 
-    int [] numbers = new int [] {4, 5, 6, 7};
+        System.out.println(numbers[2]);
 
-    System.out.println(numbers[3]);
-
-        
     }
 }
