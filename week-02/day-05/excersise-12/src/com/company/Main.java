c24f120eb2f54106a4cd7f4d65330ddb4cfa4a7c
Andrea Malinová <an.malinova@seznam.cz>
2020-07-13 20:38:34 +0200

    HelloUser

--- week-02/day-05/excersise-12/src/com/company/Main.java
+++ week-02/day-05/excersise-12/src/com/company/Main.java
@@ -5,17 +5,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter your name: ");
         String userInput1 = scanner.nextLine();
         System.out.println("Hello, " + userInput1 + "!");
-
-
-
-
 
     }
 }
