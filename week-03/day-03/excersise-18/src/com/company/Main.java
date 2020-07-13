f9bb9319636af768c1fe1b26ae21e08e8ecfbb51
Andrea Malinová <an.malinova@seznam.cz>
2020-07-14 01:21:37 +0200

    Factorial

--- week-03/day-03/excersise-18/src/com/company/Main.java
+++ week-03/day-03/excersise-18/src/com/company/Main.java
@@ -5,15 +5,14 @@ public class Main {
     public static void main(String[] args) {
         int firstNumber = 1;
         int secondNumber = 5;
-        int result = factorio(firstNumber, secondNumber);
-        System.out.println(result);
+        System.out.println(factorio(firstNumber, secondNumber));
 
     }
 
     public static int factorio(int a, int b) {
         int factorialNumber = 1;
         for (a = 1; a <= b; a++) {
-            factorialNumber = factorialNumber * a;
+            factorialNumber *= a;
         }
         return (factorialNumber);
     }
