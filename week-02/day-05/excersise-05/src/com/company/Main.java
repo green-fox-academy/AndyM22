bd7f46e13db3ddf5b12c633f41c4a0554b58b9a4
Andrea Malinová <an.malinova@seznam.cz>
2020-07-13 20:30:16 +0200

    TwoNumbers

--- week-02/day-05/excersise-05/src/com/company/Main.java
+++ week-02/day-05/excersise-05/src/com/company/Main.java
@@ -3,7 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
         System.out.println(13 + 22);
         System.out.println(13 - 22);
@@ -11,7 +10,6 @@ public class Main {
         System.out.println(22. / 13);
         System.out.println(22 / 13);
         System.out.println(22 % 13);
-
 
     }
 }
