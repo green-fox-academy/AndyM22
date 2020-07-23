2310ec7f821f5dc05647ba11683fb2f403132d44
Andrea Malinová <an.malinova@seznam.cz>
2020-07-23 21:22:26 +0200

    Anagram with test

--- week-08/day-01/excersise-03/src/com/company/MainTest.java
+++ week-08/day-01/excersise-03/src/com/company/MainTest.java
@@ -1,22 +1,28 @@
 package com.company;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
-    Main main;
+  /*  Main main;
 
-    @BeforeAll
+    @BeforeEach
     public void setUp(){
-        main = new Main();
-    }
+        this.main = new Main();
+    }*/
 
     @Test
     public void anagramShouldReturnTrue(){
-        assertTrue(true, Main.anagram("Ahoj", "joha"));
+        assertTrue(Main.anagram("Ahoj", "jo ha"));
+    }
+
+    @Test
+    public void anagramShouldReturnFalse(){
+        assertFalse(Main.anagram("jahoda", "achjo"));
     }
 
 }
