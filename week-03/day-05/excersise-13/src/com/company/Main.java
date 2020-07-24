5db42f69396092731259cacbbd5d2457eed48163
Andrea Malinová <an.malinova@seznam.cz>
2020-07-24 21:41:34 +0200

    Product database

--- week-03/day-05/excersise-13/src/com/company/Main.java
+++ week-03/day-05/excersise-13/src/com/company/Main.java
@@ -1,11 +1,10 @@
 package com.company;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-import org.w3c.dom.ls.LSOutput;
-
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
-public class Main<ArrayList> {
+public class Main {
 
     public static void main(String[] args) {
 
@@ -22,49 +21,84 @@ public class Main<ArrayList> {
 
         System.out.println("The most expensive product is " + theMostExpansiveProduct(productDatabase));
 
-        System.out.println("The average price is " + theAveragePrice(productDatabase));
+        System.out.println("The average price is " + averagePrice(productDatabase));
 
-        System.out.println("The number of products below price 300 is: " + priceBelow300((productDatabase), 300));
+        System.out.println("The number of products below price 300 is: " + productsBelow300(productDatabase));
 
-        if (productDatabase.containsValue(125)) {
-            System.out.println("Yes, we can buy anything for 125 Kč");
-        } else {
-            System.out.println("No, we can't buy anything for 125 Kč");
-        }
+        System.out.println("The products we can buy for 125: " + buyFor(productDatabase, 125));
 
-
+        System.out.println("The cheapest product we can buy is: " + theCheapestProduct(productDatabase));
 
     }
 
-    public static String theMostExpansiveProduct(HashMap<String, Integer> mapa) {//metoda, která vrátí výsledek v datovém typu String; jako parametery použije nově vytvořenou HashMapu, která pracuje s vnitřními parametry String a Integer; v metodě s ní pracuje pod názvem mapa
-        int a = 0; //vytvoří inetegr s hodnotou 0, který bude představovat hodnotu, na které začneme
-        String result = ""; //vytvoří prázdný String, do kterého pak můžeme přidat výslednou hodnotu
-        for (String element : mapa.keySet()) { //pro každou dvojici key-value vytvoří nový String s názvem element (vypíše všechny klíče s příslušnými hodnotami; element představuje klíč (key))
-            if ((mapa.get(element)) > a) { //vytvoří podmínku, pod kterou nám dá foreachloop požadovaný output = hodnota klíče z HashMapy musí být větší než počáteční hodnota a(0) - pokud je tato podmínka splněna, pokračuje dále (pokud ne, přistupuje k další dvojici key-value v seznamu HashMapy)
-                a = mapa.get(element); //počáteční hodnota a(0) je přepsána na hodnotu klíče => v dalším kole už porovnává s touto hodnotou
-                result = element; //původní hodnotu stringu přepisuje na klíč; jako poslední vyjde klíč s nejvyšší hodnotou
+    public static int averagePrice(HashMap<String, Integer> hashMap) {
+        int allPrices = 0;
+        for (int price : hashMap.values()) {
+            allPrices += price;
+        }
+        return allPrices / hashMap.size();
+    }
+
+    public static String theMostExpansiveProduct(HashMap<String, Integer> hashMap) {
+        int biggestPrice = 0;
+        String nameOfProduct = "";
+
+        for (Map.Entry<String, Integer> string : hashMap.entrySet()) {
+            int price = string.getValue();
+            String name = string.getKey();
+            if (price > biggestPrice) {
+                biggestPrice = price;
+                nameOfProduct = name;
             }
         }
-        return result; //vrací nám string s názvem klíče s njevyšší hodnotou
+        return nameOfProduct + " for " + biggestPrice;
     }
 
-
-    public static Integer theAveragePrice(HashMap<String, Integer> mapa) {
-        int a = 0; //počáteční hodnota
-        for (String element : mapa.keySet()) {
-            a += mapa.get(element); //k hodnotě integeru a(0) přičte hodnotu klíče z HashMapy => v dalším kolem loopu bude počáteční hodnota a začínat na hodnotě předchozího klíče => proces se opakuje, dokud neprojde všechny dvojice z HashMapy => výsledkem je součet všech klíčových hodnot
-        }
-        return a / mapa.size(); //vrátí součet všech hodnot vydělený počtem dvojic v HashMapě
-
-    }
-
-    public static int priceBelow300(HashMap<String, Integer> mapa, int price) {
-        int result = 0; //počáteční částka
-        for (int value : mapa.values()) { //pro každou hodnotu vytvoří nový ineteger s názvem value (vypíše všechny hodnoty)
-            if (value <= price) { //pokud je hodnota menší než stanovená částka
-                result++; //tak chci, aby se počáteční částka zvýšila o 1 => dostaneme počet hodnot, které jsou vyšší než stanovená částka
+    public static int theBiggestPrice(HashMap<String, Integer> hashMap) {
+        int biggestPrice = 0;
+        for (int price : hashMap.values()) {
+            if (price > biggestPrice) {
+                biggestPrice = price;
             }
         }
-        return result;
+        return biggestPrice;
     }
+
+    public static String theCheapestProduct(HashMap<String, Integer> hashMap) {
+        int smallestPrice = theBiggestPrice(hashMap);
+        String nameOfProduct = "";
+
+        for (Map.Entry<String, Integer> string : hashMap.entrySet()) {
+            int price = string.getValue();
+            String name = string.getKey();
+            if (price < smallestPrice) {
+                smallestPrice = price;
+                nameOfProduct = name;
+            }
+        }
+        return nameOfProduct + " for " + smallestPrice;
+    }
+
+    public static int productsBelow300(HashMap<String, Integer> hashMap) {
+        int numberOfProducts = 0;
+        for (int price : hashMap.values()) {
+            if (price < 300) {
+                numberOfProducts += 1;
+            }
+        }
+        return numberOfProducts;
+    }
+
+    public static ArrayList<String> buyFor(HashMap<String, Integer> hashMap, int price) {
+        ArrayList<String> products = new ArrayList<>();
+        for (Map.Entry<String, Integer> string : hashMap.entrySet()) {
+            int number = string.getValue();
+            String name = string.getKey();
+            if (number == price) {
+                products.add(name);
+            }
+        }
+        return products;
+    }
+
 }
