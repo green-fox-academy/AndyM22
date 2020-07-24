0c5cadc4e38eede6645352d09d7ea8c7823c3eb8
Andrea Malinová <an.malinova@seznam.cz>
2020-07-24 20:12:38 +0200

    Map introduction 2

--- week-03/day-05/excersise-09/src/com/company/Main.java
+++ week-03/day-05/excersise-09/src/com/company/Main.java
@@ -1,6 +1,9 @@
 package com.company;
 
+import com.sun.javafx.logging.JFRInputEvent;
+
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
@@ -14,23 +17,30 @@ public class Main {
         introduction.put("978-1-60309-461-0", "The Lab");
 
         for (String i : introduction.keySet()) {
-            System.out.println(introduction.get(i) + " (" + "ISBN: " + i + ")");
+            System.out.println(introduction.get(i) + " (" + "ISBN: " + i + ")"); //get(i) = get Object key
         }
 
+        //Remove the key-value pair with key 978-1-60309-444-3
         introduction.remove("978-1-60309-444-3");
 
-        introduction.remove("978-1-60309-461-0", "The Lab");
+        //Remove the key-value pair with value The Lab
+        for (String string : introduction.keySet()) {
+            if (introduction.get(string).contains("The Lab")) {
+                introduction.remove(string);
+            }
+        }
 
         introduction.put("978-1-60309-450-4", "They Called Us Enemy");
         introduction.put("978-1-60309-453-5", "Why Did We Trust Him?");
 
-
+        //Print whether there is an associated value with key 478-0-61159-424-8 or not
         if ((introduction.get("478-0-61159-424-8") == null)) {
             System.out.println("There is no associated value");
         } else {
             System.out.println("There is associated value");
         }
 
+        //Print the value associated with key 978-1-60309-453-5
         System.out.println(introduction.get("978-1-60309-453-5"));
 
     }
