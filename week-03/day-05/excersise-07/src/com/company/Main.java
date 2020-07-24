874e076502e34c81f7b5d4e0308c20dd1df7cff1
Andrea Malinová <an.malinova@seznam.cz>
2020-07-24 19:39:15 +0200

    Map introduction 1

--- week-03/day-05/excersise-07/src/com/company/Main.java
+++ week-03/day-05/excersise-07/src/com/company/Main.java
@@ -8,8 +8,6 @@ public class Main {
 
         HashMap<Integer, String> mapOfIntroduction = new HashMap<>();
 
-        System.out.println(mapOfIntroduction);
-
         mapOfIntroduction.put(97, "a");
         mapOfIntroduction.put(98, "b");
         mapOfIntroduction.put(99, "c");
@@ -17,32 +15,28 @@ public class Main {
         mapOfIntroduction.put(66, "B");
         mapOfIntroduction.put(67, "C");
 
-        for (Integer i : mapOfIntroduction.keySet()) {
-            System.out.println(i);
-        }
+        //Vytiskni seznam všech klíčů
+        System.out.println(mapOfIntroduction.keySet());
 
-        for (String i : mapOfIntroduction.values()) {
-            System.out.println(i);
-        }
+        //Vytiskni seznam hodnot
+        System.out.println(mapOfIntroduction.values());
 
+        //Add value D with the key 68
         mapOfIntroduction.put(68, "D");
+
+        //Remove the key-value pair where with key 97
+        mapOfIntroduction.remove(97);
+
+        //Print whether there is an associated value with key 100 or not
+        System.out.println(mapOfIntroduction.containsKey(100));
+
+        System.out.println(mapOfIntroduction.entrySet());
+
+        //Remove all the key-value pairs
+        mapOfIntroduction.clear();
 
         System.out.println(mapOfIntroduction.size());
 
-        System.out.println(mapOfIntroduction.get(99));
-
-        mapOfIntroduction.remove(97);
-
-        if (mapOfIntroduction.containsKey(100)) {
-            System.out.println("HashMap contains key 100");
-        } else {
-            System.out.println("HashMap does not contain key 100");
-        }
-
-        mapOfIntroduction.clear();
-
-        System.out.println(mapOfIntroduction);
     }
-
 }
 
