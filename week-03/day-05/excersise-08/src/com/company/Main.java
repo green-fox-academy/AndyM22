c1212020c5be3a8a3335f61486939d719e4d17ff
Andrea Malinová <an.malinova@seznam.cz>
2020-07-24 19:53:44 +0200

    List introduction 2

--- week-03/day-05/excersise-08/src/com/company/Main.java
+++ week-03/day-05/excersise-08/src/com/company/Main.java
@@ -16,32 +16,34 @@ public class Main {
 
         ArrayList<String> listB = new ArrayList<>();
 
+        //Create a new list ('List B') with the values of List A
         listB.addAll(listA);
 
-        if (listA.contains("Durian")) {
-            System.out.println("listA contains Durian");
-        } else {
-            System.out.println("listA does not containt Durian");
-        }
+        //Print out whether List A contains Durian or not
+        System.out.println("ListA contains Durian: " + listA.contains("Durian"));
 
+        //Remove Durian from List B
         listB.remove("Durian");
 
+        //Add Kiwifruit to List A after the 4th element
         listA.add(4, "Kiwifruit");
 
-        System.out.println("Size of listA is same as size of listB = " + listB.equals(listA));
+        //Compare the size of List A and List B
+        System.out.println("ListA is the same size as listB: " + listA.equals(listB));
 
-        System.out.println("Index of Avocado is: " + listA.indexOf("Avocado"));
+        //Get the index of Avocado from List A
+        System.out.println("Index of Avocado: " + listA.indexOf("Avocado"));
 
+        //Get the index of Durian from List B
         System.out.println("Index of Durian is: " + listB.indexOf("Durian"));
 
-        if ((listB.contains("Passion fruit")) != true) {
-            listB.add("Passion fruit");
-            listB.add("Pomelo");
-        }
+        //Add Passion Fruit and Pomelo to List B in a single statement ??
+        listB.add("Passion fruit");
+        listB.add("Pomelo");
 
+        //Print out the 3rd element from List A
         System.out.println(listA.get(2));
 
     }
-
 
 }
