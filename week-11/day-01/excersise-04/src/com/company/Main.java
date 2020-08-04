6f2c4e2093b87ebe1f7d49686baedca64fd7622f
Andrea Malinova <an.malinova@seznam.cz>
2020-08-04 21:22:31 +0200

    Excersise4

---
+++ week-11/day-01/excersise-04/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Write a Stream Expression to get the average value of the odd numbers from the following list:
+
+        List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+
+        Optional<Integer> averageValueOfOddNumbers = numbers.stream()
+                .filter(number -> number % 2 != 0)
+                .reduce(Integer::sum);
+
+        System.out.println(averageValueOfOddNumbers);
+
+    }
+}
