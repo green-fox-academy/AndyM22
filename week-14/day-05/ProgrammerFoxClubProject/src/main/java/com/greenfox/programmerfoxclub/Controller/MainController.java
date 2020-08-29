1839a9981012b1c84791e3a67d4e01a41d24a91a
Andrea Malinova <an.malinova@seznam.cz>
2020-08-29 15:59:29 +0200

    Login added

--- week-14/day-05/ProgrammerFoxClubProject/src/main/java/com/greenfox/programmerfoxclub/Controller/MainController.java
+++ week-14/day-05/ProgrammerFoxClubProject/src/main/java/com/greenfox/programmerfoxclub/Controller/MainController.java
@@ -1,7 +1,10 @@
 package com.greenfox.programmerfoxclub.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -9,6 +12,16 @@ public class MainController {
     @GetMapping("/")
     public String mainPage(){
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String getLogin(){
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String postLogin(@RequestParam String name) {
+        return "redirect:/" + name;
     }
 
 }
