764cf97db57ad5abfd6d42cc0f9bea79c85ddb8e
Andrea Malinova <an.malinova@seznam.cz>
2020-09-26 17:11:15 +0200

    deleted old redditApp

--- week-16/day-05/redditProject/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO post (title, url, created_at) VALUES('Some very interesting article', 'http://seznam.cz/', '2020-09-15');
-INSERT INTO post (title, url, created_at) VALUES('Some uninteresting article', 'http://google.cz/', '2020-09-15');
-INSERT INTO post (title, url, created_at) VALUES('Some article', 'http://nyan.cat', '2020-09-15');
-INSERT INTO post (title, url, created_at) VALUES('Article', 'http://pepa.vyskup.com/', '2020-09-15');
